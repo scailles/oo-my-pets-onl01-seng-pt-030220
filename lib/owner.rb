d066c9e5918073c215d69ff26678fdf2f0db3ce0
Shaina C <scailles23@gmail.com>
2020-04-30 00:56:06 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,32 @@
 class Owner
-  # code goes here
+  attr_accessor :pets
+  attr_reader :species, :name
+  
+  @@all=[]
+  @@count = 0
+
+def initialize(name, species = "human")
+  @name = name
+  @species = species
+  @@all << self
+  @@count += 1
+end
+
+def say_species
+  "I am a human."
+end
+
+def self.all
+  @@all
+end
+
+def self.count
+  @@count
+end
+
+def self.reset_all
+  @@all.clear
+  @@count = 0
+end
+
 end
